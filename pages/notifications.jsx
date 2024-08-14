6bd0a1a1a48a0fe9fa2c4cac234f6db67e5fe402
Parallel-EDU <ouremail@example.com>
2024-08-14 11:48:35 +0530

    Notification Done

--- pages/notifications.jsx
+++ pages/notifications.jsx
@@ -1,19 +1,19 @@
 import Navbar from "../components/navbar";
 import { useRef, useEffect, useState } from "react";
 import Image from "next/image";
-import axios from 'axios';
+import axios from "axios";
 
 function Notifications() {
   const [notifications, setNotifications] = useState([]);
-  const [selectedNotification, setSelectedNotification] = useState(null);
-  const [notification, setNotification] = useState(false);
+  const [selectedNotification, setSelectedNotification] = useState(true);
+  const [notification, setNotification] = useState(true);
   const scrollRef = useRef(null);
 
   const formatTime = (time) => {
     const date = new Date(time);
-    return date.toLocaleTimeString('en-US', {
-      hour: '2-digit',
-      minute: '2-digit',
+    return date.toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
       hour12: true,
     });
   };
@@ -39,69 +39,140 @@ function Notifications() {
 
   return (
     <>
-    <Navbar />
-    <section className="pt-[51.08px] max-sm:py-[30px] max-md:px-[40px] max-sm:px-[20px] px-[60px] pb-[123px]">
-      <h1 className="text-[20px] leading-[26px] font-semibold mb-[28.92px]">
-        Notifications
-      </h1>
-      <div className="max-md:relative">
-        <div className="w-full max-xl:w-[50%] max-md:w-full h-[567px] rounded-[5px] bg-[white] pt-[0px] pl-[41px] max-sm:px-[20px] pr-[102.28px]">
-          <div
-            ref={scrollRef}
-            className="h-[505px] max-xl:w-[90%] max-sm:w-full max-xl:overflow-x-hidden overflow-y-scroll w-full mt-[31px]"
-          >
-            {notifications.map((notif, index) => (
-              <div key={index}>
-                <h1 className="text-[16px] leading-[20.8px] mb-[16px]">
-                  {notif.date}
-                </h1>
-                <div
-                  onClick={() => handleNotificationClick(notif)}
-                  className="py-[10px] h-[75px] w-full max-xl:w-full border-b-[1px] border-[#C4C4C4] cursor-pointer"
-                >
-                  <h1 className="capitalize max-xl:truncate text-[20px] leading-[26px]">
-                    {notif.title}
-                  </h1>
-                  <p className="mt-[11px] text-[14px] leading-[18.2px] opacity-60">
-                    {formatTime(notif.time)}
-                  </p>
-                </div>
+      <Navbar />
+      <section className="pt-[51.08px] max-sm:py-[30px] max-md:px-[40px] max-sm:px-[20px] px-[60px] pb-[123px]">
+        {!notification && (
+          <>
+            <h1 className="text-[20px] leading-[26px] font-semibold mb-[28.92px]">
+              Notifications
+            </h1>
+            <div className="w-full max-xl:w-[50%] max-md:w-full h-[567px] rounded-[5px] bg-[white] pt-[0px] pl-[37px] max-sm:px-[20px] pr-[61px]">
+              <div
+                ref={scrollRef}
+                className="h-[505px] max-xl:w-[90%] max-sm:w-full max-xl:overflow-x-hidden overflow-y-scroll w-full mt-[31px]"
+              >
+                {notifications.map((notif, index) => (
+                  <div className="mt-[37px]" key={index}>
+                    <h1 className="text-[16px] leading-[20.8px] pb-[16px] border-b-[1px]">
+                      {notif.date}
+                    </h1>
+                    <div
+                      onClick={() => handleNotificationClick(notif)}
+                      className="py-[10px] flex items-center pr-[15px] justify-between h-[82px] w-full max-xl:w-full border-b-[1px] border-[#C4C4C4]"
+                    >
+                      <div className="flex gap-[24px] items-center">
+                        <div className="w-[645.83px] flex gap-[37px] items-center">
+                          <div className=" h-[34px] text-[14px] leading-[18.2px] text-white rounded-[24px] bg-[#0C6926] p-[8px]">
+                            New
+                          </div>
+                          <h1 className="capitalize text-[20px] leading-[26px] max-xl:truncate">
+                            {notif.title}
+                          </h1>
+                        </div>
+                        <p className="text-[14px] leading-[18.2px] opacity-60">
+                          {formatTime(notif.time)}
+                        </p>
+                      </div>
+
+                      <div
+                        onClick={() => setnotification(true)}
+                        className="border-[1px] w-[131px] h-[29px] text-[14px] border-black py-[5px] px-[10px] rounded-[4px] cursor-pointer opacity-1"
+                      >
+                        Join with Meet
+                      </div>
+                    </div>
+                  </div>
+                ))}
+                {/* <div className="mt-[31px]">
+                //   <h1 className="text-[16px] leading-[20.8px] mb-[16px]">
+                //     Older Notifications
+                //   </h1>
+                //   <div className="py-[10px] flex items-center pr-[15px] justify-between h-[82px] w-full max-xl:w-full border-b-[1px] border-[#C4C4C4]">
+                //     <div className="flex gap-[24px] items-center">
+                //       <div className="w-[645.83px] flex gap-[37px] items-center">
+                //         <div className=" h-[34px] text-[14px] leading-[18.2px] text-white rounded-[24px] bg-[#0C6926] p-[8px] opacity-0 cursor-arrow">
+                //           New
+                //         </div>
+                //         <h1 className="capitalize text-[20px] leading-[26px] max-xl:truncate">
+                //           Justo est urna pellentesque commodo quis eget.
+                //         </h1>
+                //       </div>
+                //       <p className="text-[14px] leading-[18.2px] opacity-60">
+                //         13th July
+                //       </p>
+                //     </div>
+                //   </div> */}
+                {/* </div> */}
               </div>
-            ))}
-          </div>
-        </div>
+            </div>{" "}
+            <div className="flex gap-[16px] absolute right-[60px] mt-[20px]">
+              <div className="w-[32px] h-[32px] flex pr-[2.98px] justify-center items-center border-[1.23px] border-[#00000033] cursor-pointer">
+                <Image src="/Group 4.svg" width={14.13} height={14.13} />
+              </div>
+              <div className="flex gap-[8px]">
+                <p className="w-[32px] text-[17.23px] cursor-pointer bg-[#31E39E] leading-[16px] h-[32px] flex justify-center items-center text-black border-[#000]">
+                  1
+                </p>
+                <p className="w-[32px] text-[17.23px] leading-[16px] h-[32px] flex justify-center items-center border-[1.23px] border-[#00000033] cursor-pointer">
+                  2
+                </p>
+                <p className="w-[32px] text-[17.23px] leading-[16px] h-[32px] flex justify-center items-center border-[1.23px] border-[#00000033] cursor-pointer">
+                  3
+                </p>
+                <p className="w-[25px] text-[17.23px] leading-[16px] h-[32px] flex justify-center items-center cursor-pointer"></p>
+                <p className="w-[32px] text-[17.23px] leading-[16px] h-[32px] flex justify-center items-center border-[1.23px] border-[#00000033] cursor-pointer">
+                  42
+                </p>
+              </div>
+              <div className="w-[32px] rotate-180 h-[32px] flex pr-[2.98px] justify-center items-center border-[1.23px] border-[#00000033] cursor-pointer">
+                <Image src="/Group 4.svg" width={14.13} height={14.13} />
+              </div>
+            </div>
+          </>
+        )}
 
         {notification && selectedNotification && (
-          <div className="h-[561px] max-sm:p-[20px] max-xl:w-[50%] max-md:w-full max-md:absolute w-[calc(100%-782px)] bg-[white] rounded-[5px] mt-[6px] pt-[38.5px] pl-[23.5px] pr-[43.5px]">
-            <h1 className="capitalize max-hamburger:w-[90%] text-[20px] leading-[26px] font-semibold">
-              {selectedNotification.title}
-            </h1>
-            <div className="mt-[16px] max-hamburger:mb-[10px] flex gap-[29.43px] mb-[42.08px]">
-              <p className="text-[14px] leading-[18.2px] opacity-60">
-                {formatTime(selectedNotification.time)}
-              </p>
-            </div>
-            <p className="text-base max-hamburger:mb-[20px] mb-[36.61px]">
-              {selectedNotification.description}
-            </p>
-            <button className="h-[43px] max-sm:w-full w-[134px] bg-[#0559BB] text-white rounded-[6px] text-[17.95px] leading-[21.54px]">
-              Join in zoom
-            </button>
-            <span
+          <>
+            <Image
+              src="back.svg"
+              className="mb-[28.02px] cursor-pointer"
               onClick={() => setNotification(false)}
-              className="hidden max-md:block"
-            >
-              <Image
-                src="/Vector (2).svg"
-                className="absolute max-md:right-[40px] max-sm:right-[20px] right-[50.33px] cursor-pointer top-[37.29px]"
-                width={23.35}
-                height={23.35}
-              />
-            </span>
-          </div>
+              width={44.97}
+              height={44.97}
+            />
+            <div className="max-sm:p-[20px] w-full bg-[white] rounded-[5px] pt-[48.5px] pl-[50px] pb-[136px] pr-[43.5px]">
+              <h1 className="capitalize text-[31px] leading-[37px]">
+                {selectedNotification.title}
+              </h1>
+              <div className="mt-[16px] max-hamburger:mb-[10px] flex gap-[29.43px] mb-[42.08px]">
+                <p className="text-[14px] leading-[18.2px] opacity-60">
+                  {formatTime(selectedNotification.time)}
+                </p>
+                <p className="text-[14px] leading-[18.2px] opacity-60">
+                  12th April 2024
+                </p>
+              </div>
+              <p className="text-base max-hamburger:mb-[20px] mb-[42px] w-[808px]">
+                {selectedNotification.description}
+              </p>
+              <button className="h-[43px] max-sm:w-full w-[134px] bg-[#30E29D] rounded-[6px] text-base font-semibold">
+                Join in zoom
+              </button>
+              <span
+                onClick={() => setNotification(false)}
+                className="hidden max-md:block"
+              >
+                <Image
+                  src="/Vector (2).svg"
+                  className="absolute max-md:right-[40px] max-sm:right-[20px] right-[50.33px] cursor-pointer top-[37.29px]"
+                  width={23.35}
+                  height={23.35}
+                />{" "}
+              </span>
+            </div>
+          </>
         )}
-      </div>
-    </section>
+      </section>
     </>
   );
 }
